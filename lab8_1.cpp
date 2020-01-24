da1d623b61ecb79871e002a9aafa155ac3b10df9
Sirapop Para <worwee07@MacBook-Pro.local>
2020-01-24 15:52:41 +0700

    editted

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -3,28 +3,46 @@
 using namespace std;
 
 int main(){	
+	int i=1;
+	float loan=0,rate=0,pay=0;
+	float total=0,balance;
+	cout << "Enter initial loan: ";
+	cin >> loan;
+	cout << "Enter interest rate per year (%): ";
+	cin >> rate;
+	cout << "Enter amount you can pay per year: ";
+	cin >> pay;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
 	//Try to change from 'left' to 'right' and see the effect
-	cout << setw(13) << left << "EndOfYear#"; 
-	cout << setw(13) << left << "PrevBalance"; 
-	cout << setw(13) << left << "Interest"; 
-	cout << setw(13) << left << "Total";
-	cout << setw(13) << left << "Payment";
-	cout << setw(13) << left << "NewBalance";
+	cout << setw(15) << left << "EndOfYear#"; 
+	cout << setw(15) << left << "PrevBalance"; 
+	cout << setw(15) << left << "Interest"; 
+	cout << setw(15) << left << "Total";
+	cout << setw(15) << left << "Payment";
+	cout << setw(15) << left << "NewBalance";
 	cout << "\n";
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	do
+	{
+		cout << fixed << setprecision(2); 
+		cout << setw(13) << left << i; 
+		cout << setw(13) << left << loan;
+		cout << setw(13) << left << loan*rate/100;
+		total = loan+(loan*rate/100);
+		if(total<pay) pay = total;
+		cout << setw(13) << left << total;
+		cout << setw(13) << left << pay;
+		balance = total - pay;
+		cout << setw(13) << left << balance;
+		cout << "\n";	
+		loan = balance;
+		i++;
+	} while (balance!=0);
+
 	
 	return 0;
 }
--- lab8_2.cpp
+++ lab8_2.cpp
@@ -1,7 +1,21 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 //Write the function printO() here
+void printO(int N,int M){
+    int i,j;
+    if(N<1||M<1){
+       cout << "Invalid input";
+    }else{
+        for(i=0;i<N;i++){
+            for(j=0;j<M;j++){
+                cout << "O";
+            }
+           cout << "\n";
+        }
+    }
+}
 
 int main(){
 	
